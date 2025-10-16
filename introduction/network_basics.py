42e48e66f3e13a6b4d23dd8e520e76e9b9f5c7c4
Sint.soft <sint.soft@yandex.ru>
2025-10-16 20:56:20 +0300

    Network basics

---
+++ introduction/network_basics.py
@@ -0,0 +1,22 @@
+'''
+Several of the challenges are dynamic and require you to talk to our challenge servers over the network. This allows you to perform man-in-the-middle attacks on people trying to communicate, or directly attack a vulnerable service. To keep things consistent, our interactive servers always send and receive JSON objects.
+
+Such network communication can be made easy in Python with the pwntools module. This is not part of the Python standard library, so needs to be installed with pip using the command line pip install pwntools.
+
+For this challenge, connect to socket.cryptohack.org on port 11112. Send a JSON object with the key buy and value flag.
+'''
+
+
+import socket
+from http.client import responses
+
+sc = socket.socket()
+sc.connect(('socket.cryptohack.org', 11112))
+
+
+sc.send(b'{ "buy": "flag" }')
+responses = sc.recv(1024)
+
+print(responses)
+
+sc.close()
